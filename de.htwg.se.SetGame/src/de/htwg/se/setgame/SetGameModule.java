20c692a3b35cc1abaf0561c62e9f86a2023d4f9b
Philipp Daniels <philipp.daniels@gmail.com>
2015-04-01 10:46:25 +0200

    Add test for SetGameModule

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/SetGameModule.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/SetGameModule.java
@@ -1,18 +1,17 @@
 package de.htwg.se.setgame;
 
 import com.google.inject.AbstractModule;
-
 import de.htwg.se.setgame.controller.IController;
+import de.htwg.se.setgame.controller.impl.SetController;
 import de.htwg.se.setgame.model.IField;
+import de.htwg.se.setgame.model.impl.Field;
 
 public class SetGameModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(IField.class).to(de.htwg.se.setgame.model.impl.Field.class);
-		bind(IController.class).to(
-				de.htwg.se.setgame.controller.impl.SetController.class);
-
+		bind(IField.class).to(Field.class);
+		bind(IController.class).to(SetController.class);
 	}
 
 }
