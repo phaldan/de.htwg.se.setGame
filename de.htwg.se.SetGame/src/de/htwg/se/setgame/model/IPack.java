989112005ea718e3611d6d9c35389e8ffdf6e5e9
Philipp Daniels <philipp.daniels@gmail.com>
2015-04-07 19:53:36 +0200

    Extend ICard with isSet(...) method

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/model/IPack.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/model/IPack.java
@@ -11,4 +11,12 @@ public interface IPack {
      * @return pack of cards
      */
     List<ICard> getPack();
+
+    /**
+     * @param one Instance of ICard
+     * @param two Instance of ICard
+     * @param three Instance of ICard
+     * @return Returns true, when all three cards together are a valid set.
+     */
+    boolean isSet(ICard one, ICard two, ICard three);
 }
