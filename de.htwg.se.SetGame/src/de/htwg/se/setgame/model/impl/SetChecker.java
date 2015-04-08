6c9a14e130d10d9044ecc044f447f70b153c7dfc
Philipp Daniels <philipp.daniels@gmail.com>
2015-04-08 15:51:50 +0200

    Extend SetChecker with check(...)

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/model/impl/SetChecker.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/model/impl/SetChecker.java
@@ -2,6 +2,8 @@ package de.htwg.se.setgame.model.impl;
 
 import de.htwg.se.setgame.model.ICard;
 
+import java.util.List;
+
 /**
  * @author Philipp Daniels
  */
@@ -11,7 +13,7 @@ public class SetChecker {
      * @param one Instance of ICard
      * @param two Instance of ICard
      * @param three Instance of ICard
-     * @return Returns true, when all three cards together are a valid set.
+     * @return Returns true, when all three cards are together a valid set.
      */
     public static boolean isSet(ICard one, ICard two, ICard three) {
         return compare(one.getColor(), two.getColor(), three.getColor())
@@ -23,5 +25,50 @@ public class SetChecker {
     private static boolean compare(Object one, Object two, Object three) {
         return one.equals(two) && one.equals(three) && two.equals(three)
                 || !one.equals(two) && !one.equals(three) && !two.equals(three);
+    }
+
+    private List<ICard> cards;
+
+    /**
+     * @param cards List of cards
+     * @return Returns false, when list of cards has not a single valid set.
+     */
+    public boolean check(List<ICard> cards) {
+        this.cards = cards;
+        return hasSetOne();
+    }
+
+    private boolean hasSetOne() {
+        for (int i=0; i< cards.size()-2; i++) {
+            if (hasSetTwo(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasSetTwo(int one) {
+        for (int i=one + 1; i < cards.size()-1; i++) {
+            if (hasSetThree(one, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean hasSetThree(int oneI, int twoI) {
+        for (int i=twoI+1; i < cards.size(); i++) {
+            if (checkSet(oneI, twoI, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkSet(int oneI, int twoI, int threeI) {
+        ICard one = cards.get(oneI);
+        ICard two = cards.get(twoI);
+        ICard three = cards.get(threeI);
+        return SetChecker.isSet(one, two, three);
     }
 }
--- de.htwg.se.SetGame/test/de/htwg/se/setgame/model/impl/SetCheckerTest.java
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/model/impl/SetCheckerTest.java
@@ -1,6 +1,11 @@
 package de.htwg.se.setgame.model.impl;
 
+import de.htwg.se.setgame.model.ICard;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -9,6 +14,13 @@ import static org.junit.Assert.assertTrue;
  * @author Philipp Daniels
  */
 public class SetCheckerTest {
+
+    private SetChecker target;
+
+    @Before
+    public void setUp() {
+        target = new SetChecker();
+    }
 
     @Test
     public void isSet_success_allSame() {
@@ -93,6 +105,34 @@ public class SetCheckerTest {
         Card one   = new Card(Card.COLOR_1, Card.FORM_1, Card.FILL_1, Card.COUNT_1);
         Card two   = new Card(Card.COLOR_2, Card.FORM_2, Card.FILL_2, Card.COUNT_3);
         Card three = new Card(Card.COLOR_3, Card.FORM_3, Card.FILL_3, Card.COUNT_3);
+        assertFalse(SetChecker.isSet(one, two, three));
+    }
+
+    @Test
+    public void check_success_threeCards() {
+        List<ICard> list = new LinkedList<>();
+        list.add(new Card(Card.COLOR_1, Card.FORM_1, Card.FILL_1, Card.COUNT_1));
+        list.add(new Card(Card.COLOR_2, Card.FORM_2, Card.FILL_2, Card.COUNT_2));
+        list.add(new Card(Card.COLOR_3, Card.FORM_3, Card.FILL_3, Card.COUNT_3));
+        assertTrue(target.check(list));
+    }
+
+    @Test
+    public void check_success_fourCards() {
+        List<ICard> list = new LinkedList<>();
+        list.add(new Card(Card.COLOR_1, Card.FORM_1, Card.FILL_1, Card.COUNT_1));
+        list.add(new Card(Card.COLOR_1, Card.FORM_2, Card.FILL_2, Card.COUNT_2));
+        list.add(new Card(Card.COLOR_2, Card.FORM_2, Card.FILL_2, Card.COUNT_2));
+        list.add(new Card(Card.COLOR_2, Card.FORM_3, Card.FILL_3, Card.COUNT_3));
+        list.add(new Card(Card.COLOR_3, Card.FORM_3, Card.FILL_3, Card.COUNT_3));
+        assertTrue(target.check(list));
+    }
+
+    @Test
+    public void check_fail() {
+        Card one   = new Card(Card.COLOR_1, Card.FORM_1, Card.FILL_1, Card.COUNT_1);
+        Card two   = new Card(Card.COLOR_1, Card.FORM_2, Card.FILL_2, Card.COUNT_2);
+        Card three = new Card(Card.COLOR_2, Card.FORM_3, Card.FILL_3, Card.COUNT_3);
         assertFalse(SetChecker.isSet(one, two, three));
     }
 }
