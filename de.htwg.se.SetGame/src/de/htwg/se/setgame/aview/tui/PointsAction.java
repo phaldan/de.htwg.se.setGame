500f369eccb677b77e0fa11cc4cbfca5ec7407a7
Philipp Daniels <philipp.daniels@gmail.com>
2015-04-10 12:31:51 +0200

    Add PointsAction class

---
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/aview/tui/PointsAction.java
@@ -0,0 +1,32 @@
+package de.htwg.se.setgame.aview.tui;
+
+import de.htwg.se.setgame.controller.IController;
+
+/**
+ * @author Philipp Daniels
+ */
+public class PointsAction extends Action {
+
+    public static final String CMD = "p";
+    public static final String DESC = "If you want to know the game point";
+    public static final String OUTPUT = "Player one = %s%nPlayer Two = %s%n";
+
+    public PointsAction(IController controller) {
+        super(controller);
+    }
+
+    @Override
+    public String getCommand() {
+        return PointsAction.CMD;
+    }
+
+    @Override
+    public String getDescription() {
+        return PointsAction.DESC;
+    }
+
+    @Override
+    public String execute(String[] input) {
+        return String.format(PointsAction.OUTPUT, controller.getPlayerOnePoints(), controller.getPlayerTwoPoints());
+    }
+}
---
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/aview/tui/PointsActionTest.java
@@ -0,0 +1,57 @@
+package de.htwg.se.setgame.aview.tui;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Philipp Daniels
+ */
+public class PointsActionTest {
+
+    private PointsAction target;
+    private int pointsOne;
+    private int pointsTwo;
+
+    private class Controller extends ControllerDummy {
+
+        @Override
+        public int getPlayerOnePoints() {
+            return pointsOne;
+        }
+
+        @Override
+        public int getPlayerTwoPoints() {
+            return pointsTwo;
+        }
+    }
+
+    @Before
+    public void setUp() {
+        target = new PointsAction(new Controller());
+    }
+
+    @Test
+    public void getCommand_success() {
+        assertNotNull(target.getCommand());
+        assertEquals(PointsAction.CMD, target.getCommand());
+    }
+
+    @Test
+    public void getDescription_success() {
+        assertNotNull(target.getDescription());
+        assertEquals(PointsAction.DESC, target.getDescription());
+    }
+
+    @Test
+    public void execute_success() {
+        pointsOne = 12;
+        pointsTwo = 5;
+
+        String result = target.execute(null);
+        assertNotNull(result);
+        assertTrue(result.contains(Integer.toString(pointsOne)));
+        assertTrue(result.contains(Integer.toString(pointsTwo)));
+        assertEquals(String.format(PointsAction.OUTPUT, pointsOne, pointsTwo), result);
+    }
+}
