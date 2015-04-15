3e5426e61f083b1d8c08815222de757981033630
Philipp Daniels <philipp.daniels@gmail.com>
2015-04-15 15:27:28 +0200

    Add override annotation to fix undocumented APIs at util.observer package

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/util/observer/Observable.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/util/observer/Observable.java
@@ -5,33 +5,42 @@ import java.util.List;
 
 /**
  * Implement IObservable with ArrayList.
+ * @author Philipp
  */
 public abstract class Observable implements IObservable {
 
 	private List<IObserver> subscribers = new ArrayList<>();
 
+	@Override
 	public void addObserver(IObserver s) {
 		subscribers.add(s);
 	}
 
+	@Override
 	public void removeObserver(IObserver s) {
 		subscribers.remove(s);
 	}
 
+	@Override
 	public void removeAllObservers() {
 		subscribers.clear();
 	}
 
+	@Override
 	public void notifyObservers() {
 		notifyObservers(null);
 	}
 
+	@Override
 	public void notifyObservers(Event e) {
 		for (IObserver o: subscribers) {
 			o.update(e);
 		}
 	}
 
+	/**
+	 * @return Return number of IObserver instances
+	 */
 	public int countObservers() {
 		return subscribers.size();
 	}
