cfe56a4b6a517bd4816d39c9cc44f09c2b7254d5
Philipp Daniels <philipp.daniels@gmail.com>
2015-04-15 12:27:13 +0200

    Ignore TestAppender as test-class

--- de.htwg.se.SetGame/test/de/htwg/se/setgame/TestAppender.java
+++ de.htwg.se.SetGame/test/de/htwg/se/setgame/TestAppender.java
@@ -2,6 +2,7 @@ package de.htwg.se.setgame;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.spi.LoggingEvent;
+import org.junit.Ignore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * @author Philipp Daniels
  */
+@Ignore
 public class TestAppender extends AppenderSkeleton {
 
     private final List<LoggingEvent> log = new ArrayList<>();
