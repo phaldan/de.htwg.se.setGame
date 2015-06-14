7e5613719b8a094c2233792e9547c691483558f5
Philipp Daniels <philipp.daniels@gmail.com>
2015-06-14 16:06:48 +0200

    Extend HibernateManager with constructor to initialize SessionFactory

--- de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/util/persistence/hibernate/HibernateManager.java
+++ de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/util/persistence/hibernate/HibernateManager.java
@@ -1,11 +1,26 @@
 package de.htwg.se.setgame.util.persistence.hibernate;
 
 import de.htwg.se.setgame.util.persistence.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 /**
  * @author Philipp Daniels
  */
 public class HibernateManager implements DaoManager {
+
+    private SessionFactory factory;
+
+    public HibernateManager() {
+        Configuration cfg = new Configuration();
+        Properties prop = cfg.configure().getProperties();
+        ServiceRegistry service = new StandardServiceRegistryBuilder().applySettings(prop).build();
+        factory = cfg.buildSessionFactory(service);
+    }
 
     @Override
     public CardDao getCard() {
