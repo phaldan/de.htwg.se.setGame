cfe75ad595785a0431acfb8920beca75b1669bf8
Philipp Daniels <philipp.daniels@gmail.com>
2015-06-14 23:06:34 +0200

    Rename Game_db4o to GameDaoDb4o

---
+++ de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/util/persistence/db4o/GameDaoDb4o.java
@@ -0,0 +1,32 @@
+package de.htwg.se.setgame.util.persistence.db4o;
+
+import de.htwg.se.setgame.model.IGame;
+import de.htwg.se.setgame.util.persistence.GameDao;
+
+import java.util.List;
+
+/**
+ * @author Philipp Daniels
+ */
+public class GameDaoDb4o implements GameDao {
+
+    @Override
+    public IGame create() {
+        return null;
+    }
+
+    @Override
+    public List<IGame> getByPlayer() {
+        return null;
+    }
+
+    @Override
+    public void add(IGame game) {
+
+    }
+
+    @Override
+    public void update(IGame game) {
+
+    }
+}
--- de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/util/persistence/db4o/Game_db4o.java
+++
@@ -1,8 +0,0 @@
-package de.htwg.se.setgame.util.persistence.db4o;
-
-/**
- * Created by Pavan on 04/06/2015.
- */
-public class Game_db4o {
-
-}
