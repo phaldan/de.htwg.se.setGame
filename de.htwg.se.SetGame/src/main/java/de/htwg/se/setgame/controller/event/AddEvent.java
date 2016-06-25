5214da0cd5ca2094edf9e212c806b6775381e688
Philipp Daniels <philipp.daniels@gmail.com>
2016-06-25 20:27:30 +0200

    Extend Events with useful log-entries

--- de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/controller/event/AddEvent.java
+++ de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/controller/event/AddEvent.java
@@ -1,9 +1,16 @@
 package de.htwg.se.setgame.controller.event;
 
 import de.htwg.se.setgame.util.observer.Event;
+import org.apache.log4j.Logger;
 
 /**
  * @author Philipp Daniels
  */
 public class AddEvent implements Event {
+
+    private static final Logger LOGGER = Logger.getLogger(AddEvent.class);
+
+    public AddEvent() {
+        LOGGER.debug("Create AddEvent");
+    }
 }
--- de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/controller/event/CloseEvent.java
+++ de.htwg.se.SetGame/src/main/java/de/htwg/se/setgame/controller/event/CloseEvent.java
@@ -1,9 +1,16 @@
 package de.htwg.se.setgame.controller.event;
 
 import de.htwg.se.setgame.util.observer.Event;
+import org.apache.log4j.Logger;
 
 /**
  * @author Philipp Daniels
  */
 public class CloseEvent implements Event {
+
+    private static final Logger LOGGER = Logger.getLogger(CloseEvent.class);
+
+    public CloseEvent() {
+        LOGGER.debug("Create CloseEvent");
+    }
 }
